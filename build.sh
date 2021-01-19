2e79fc4f7453aef6e6a0119b13ff4ca2fedcb8d9
vemv <vemv@users.noreply.github.com>
2021-01-19 05:28:46 +0100

    build

--- build.sh
+++ build.sh
@@ -1,14 +1,2 @@
-#!/bin/bash
-
-# runs source-deps and tests and then provided lein target with mranderson
-
-function check_result {
-    if [ $? -ne 0 ]; then
-        echo "FAILED"
-        exit 1
-    fi
-}
-
 lein do clean, source-deps :project-prefix cider.inlined-deps
-check_result
-lein with-profile plugin.mranderson/config "$@"
+lein with-profile plugin.mranderson/config do pom, jar
